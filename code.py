37989ecca64287f566fa7be5839ce6c8cfc3d30a
Janani <123891896+Janani145@users.noreply.github.com>
2025-05-04 12:51:42 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+result = a + b
+print("Matrix A:\n", a)
+print("Matrix B:\n", b)
+print("Sum of A and B:\n", result)
